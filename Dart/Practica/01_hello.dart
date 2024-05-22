e3b6598086e59efceeabd968415c940146e3fdfd
Pepe <pepewee07@gmail.com>
2024-05-22 13:49:24 -0500

    new p

--- Dart/Practica/01_hello.dart
+++ Dart/Practica/01_hello.dart
@@ -3,6 +3,7 @@ void main() {
   // var myName = "José";
   // late final myName = "José";
   // const myName = "José";
+  // final myName = "José";
   String myName = "José";
   
   print("Helou, ${myName.toUpperCase()}");
